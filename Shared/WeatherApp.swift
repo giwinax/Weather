5fd78448a6a90f3f36fc55d2e2bc19ebcc0f1cdc
giwinax <giwinax@gmail.com>
2021-12-05 12:14:05 +0300

    Initial Commit

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by s b on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
